b31da2dc9ade18c77026e00ef0eeeb402242d47f
Gabe Jackson <gj@mail.co.de>
2020-10-25 16:43:58 -0400

    Clean up the Inverter

--- polar-core/src/vm.rs
+++ polar-core/src/vm.rs
@@ -431,7 +431,7 @@ impl PolarVirtualMachine {
             Goal::BindBatch { bindings } => bindings
                 .borrow_mut()
                 .drain(..)
-                .for_each(|Binding(var, value)| self.bind(&var, value.clone())),
+                .for_each(|Binding(var, value)| self.bind(&var, value)),
             Goal::Run { runnable } => return self.run_runnable(runnable.clone_runnable()),
         }
         Ok(QueryEvent::None)
